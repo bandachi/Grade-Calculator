4de83c0f44604c8ca0636db945fec426e4e67fe5
Hyeonseob Yang <nick_yang@sfu.ca>
2019-09-27 18:00:05 -0700

    Added paths to css and js files

--- grade-calculator-a1/index.js
+++ grade-calculator-a1/index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const VIEWS = path.join(__dirname, 'views');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
+  .set('views', VIEWS)
+  .set('view engine', 'html')
+  .use(express.static('public'))
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.sendFile('calculator.html', { root : VIEWS }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
